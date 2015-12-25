6f5657156c8417bf2b1b35bea4334cd4042ff4e4
Javier <pontiactransam@gmail.com>
2015-12-26 01:54:40 +0800

    i18n support

---
+++ src/main/java/com/example/MvcConfig.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by Javier on 02/12/2015.
+ */
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/form").setViewName("form");
+    }
+
+}
